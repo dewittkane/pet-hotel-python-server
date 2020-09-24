bc589d9099f81bd9d4efe1c46d2e87dc64054d4c
Alex Severson <alexsvrsn@gmail.com>
2020-09-24 15:44:03 -0500

    update to connection function

--- connectionFunction.py
+++ connectionFunction.py
@@ -1,4 +1,4 @@
 import psycopg2 
 import psycopg2.extras 
 
-conn = psycopg2.connect("dbname=pet_hotel user=dewitt", cursor_factory=psycopg2.extras.RealDictCursor)
+conn = psycopg2.connect("dbname=pet_hotel user=alexseverson", cursor_factory=psycopg2.extras.RealDictCursor)
